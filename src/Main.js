d9e21f4c03878416ddf3ce4840083cadff6dd5bd
nguyendaitriet96 <triet.nguyendai@smartdev.com>
2024-09-27 10:22:05 +0700

    useEffect Hook with dependencies

--- src/Main.js
+++ src/Main.js
@@ -1,13 +1,43 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { SampleTitle } from "./Components";
 
 function Main() {
     const [show, setShow] = useState(false);
+    const [title, setTitle] = useState('');
+    const [list, setList] = useState([]);
+
+    const tabs = ['todos', 'users'];
+    const [tabType, setTabType] = useState('todos')
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/${tabType}`)
+            .then(response => response.json())
+            .then(list => setList(list))
+    }, [tabType]);
 
     return (
         <div className="Main">
             <button onClick={() => setShow(!show)}>Toggle</button>
-            {show && <SampleTitle />}
+            {show &&
+                <>
+                    <SampleTitle />
+                    {
+                        tabs.map(tab =>
+                            <button
+                                onClick={() => setTabType(tab)}
+                                style={tab === tabType ? {color: "white", backgroundColor: "black"} : {}} 
+                                key={tab}
+                            >
+                                {tab}
+                            </button>
+                        )
+                    }
+                    <input onChange={(e) => setTitle(e.target.value)} />
+                    <ul>
+                        {list.map(item => <li key={item.id}>{item.title || item.name}</li>)}
+                    </ul>
+                </>
+            }
         </div>
     );
 }
